3712c8339443333ab45884292b83d98f0422d90e
AstrobioMike <michael.lee0517@gmail.com>
2020-04-01 14:30:22 -0700

    initial build

---
+++ install.R
@@ -0,0 +1,25 @@
+install.packages("vegan")
+install.packages("dendextend")
+install.packages("tidyr")
+install.packages("viridis")
+install.packages("reshape")
+
+install.packages("BiocManager")
+
+BiocManager::install("phyloseq")
+BiocManager::install("DESeq2")
+BiocManager::install("ShortRead", suppressUpdates = FALSE)
+
+biocLite("phyloseq")
+biocLite("DESeq2")
+biocLite("ShortRead", suppressUpdates = FALSE)
+
+
+install.packages("devtools")
+devtools::install_github("benjjneb/dada2")
+devtools::install_github("benjjneb/decontam")
+
+
+devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
+devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
+biocLite("dendextend")
